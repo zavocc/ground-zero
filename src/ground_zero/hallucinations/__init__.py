5bae4fdbafde0ccc3479e8ab402bdf740d35d0c7
zavocc <buffer.0verfl0w1030@gmail.com>
2026-09-18 20:34:55 +0800

    refactor
    per evaluation category is now located on source code root

---
+++ src/ground_zero/hallucinations/__init__.py
@@ -0,0 +1,4 @@
+from .api import AsyncChecker as AsyncHallucinationsChecker
+from .api import Checker as HallucinationsChecker
+
+__all__ = ["AsyncHallucinationsChecker", "HallucinationsChecker"]
---
+++ src/ground_zero/tasks/hallucinations/__init__.py
@@ -0,0 +1,4 @@
+from .simple import SimpleIOTask as SimpleTask
+from .toolcall import ToolCallIOTask as ToolCallTask
+
+__all__ = ["SimpleTask", "ToolCallTask"]
